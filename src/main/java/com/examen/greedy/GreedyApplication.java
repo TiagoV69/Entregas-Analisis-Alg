3ac80b64ac72c5e7f4e8344b6b2155f76c9ae524
Tiagov619 <santiago.varela@smartsoft.com.co>
2026-08-31 18:29:21 -0500

    Primer commit: Estructura base de Spring Boot y clases Java

---
+++ src/main/java/com/examen/greedy/GreedyApplication.java
@@ -0,0 +1,13 @@
+package com.examen.greedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreedyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreedyApplication.class, args);
+	}
+
+}
