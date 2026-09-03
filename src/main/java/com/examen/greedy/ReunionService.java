cb7a61ab0bc534baca5b399fbcca959959694a96
Brayan Alexis Correa Torres <brayan.correa@newinntech.com>
2026-09-02 21:11:53 -0500

    Validations And Structure of Methods Done

--- src/main/java/com/examen/greedy/ReunionService.java
+++ src/main/java/com/examen/greedy/ReunionService.java
@@ -1,4 +1,22 @@
 package com.examen.greedy;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReunionService {
+
+  public List<Reunion> optimizar(List<Reunion> reuniones) {
+    if (reuniones == null || reuniones.isEmpty()) {
+      return List.of();
+    }
+
+    return null;
+  }
+
+  private void validarReunion(Reunion reunion) {
+
+  }
+
 }
