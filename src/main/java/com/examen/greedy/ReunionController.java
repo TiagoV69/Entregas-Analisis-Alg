340fcd020067b6703607b37adeb27b0c9e5534a5
enriquebetancur57 <enriquebetancur577@gmail.com>
2026-09-03 13:54:13 -0500

    Se agrega ReunionController

---
+++ src/main/java/com/examen/greedy/ReunionController.java
@@ -0,0 +1,23 @@
+package com.examen.greedy;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/reuniones")
+public class ReunionController {
+
+    private final ReunionService reunionService;
+
+    public ReunionController(ReunionService reunionService) {
+        this.reunionService = reunionService;
+    }
+}
